962d51c2cabcc6e907fb4564a0ac11c07d951a3f
Carla Olivares <alu0101120218@ull.edu.es>
2022-05-07 19:10:30 +0000

    Avances

---
+++ src/message_event_emitter.ts
@@ -0,0 +1,17 @@
+import {EventEmitter} from 'events';
+
+export class MessageEventEmitter extends EventEmitter {
+  constructor(connection: EventEmitter) {
+    super();
+
+    let data = '';
+    connection.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    connection.on('end', () => {
+      const message = JSON.parse(data.toString());
+      this.emit('message', message);
+    });
+  }   
+}
---
+++ src/types.ts
@@ -0,0 +1,17 @@
+/**
+ * Formulario base de una petición
+ */
+export type RequestType = {
+  type: 'add' | 'modify' | 'remove' | 'read' | 'list';
+  user: string;
+  title?: string;
+  body?: string;
+  color?: string;
+}
+
+/**
+ * Respuesta del servidor
+ */
+export type ResponseType = {
+  message: string;
+}
